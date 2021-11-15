0d1641d1325b3bca8fbada2a74fa8a398191c68f
Andrew McPherson <andrew.mcpherson@gmail.com>
2021-11-15 16:48:02 -0500

    additional filteres

--- scripts/filter_impact_maf.R
+++ scripts/filter_impact_maf.R
@@ -6,11 +6,15 @@ maf <- vroom(snakemake@input[[1]], comment="#")
 
 #filter based on population AF, mutation impact/consequence
 maf_filt <- maf %>%
+  #filter common_variant in 
+  filter(grepl('common_variant', FILTER)) %>%
+  #filter for AF of 1%
+  filter(AF < 0.01 | is.na(AF)) %>% 
   #filter for gnomad AF of 1%
   filter(gnomAD_AF < 0.01 | is.na(gnomAD_AF)) %>% 
   #filter for protein coding or Oncogenic
   filter(!Variant_Classification %in% c("Silent", "Intron", "IGR", "3'UTR", "5'UTR", "3'Flank", "5'Flank")) %>%
-  #filter for high impact
+  #filter for impact
   filter(IMPACT %in% c("HIGH", "MODERATE", "LOW"))
 
 fwrite(maf_filt, file = snakemake@output[[1]], sep = "\t")
